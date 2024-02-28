f2f3c7989c67ea93e69018b9265d05850c3d115b
aeyga <demxultra@gmail.com>
2024-02-28 00:44:33 -0500

    - Added the Merge Sort algorithm into Algorithms - Separated public and private methods into their own regions.

--- leetcode playground/Algorithms.cs
+++ leetcode playground/Algorithms.cs
@@ -8,6 +8,93 @@ namespace leetcode_playground
 {
     public static class Algorithms
     {
+        #region Private methods
+        private static int guess(int num)
+        {
+            int pick = 1702766719;
+            if (num > pick)
+                return -1;
+            else if (num < pick)
+                return 1;
+            else
+                return 0;
+        }
+
+        /// <summary>
+        /// Split array into two halves and merge them into the input array, overwriting it with the sorted result.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="l"></param>
+        /// <param name="m"></param>
+        /// <param name="r"></param>
+        private static void Merge(int[] array, int l, int m, int r)
+        {
+            int length1 = m - l + 1;
+            int length2 = r - m;
+
+            int[] L = new int[length1];
+            int[] R = new int[length2];
+
+            for (int i = 0; i < length1; i++)
+                L[i] = array[l + 1];
+            for (int j = 0; j < length2; j++)
+                R[j] = array[m + 1 + j];
+
+            int ii = 0;
+            int jj = 0;
+            int k = l;
+
+            while (ii < length1 && jj < length2)
+            {
+                if (L[ii] <= R[jj])
+                {
+                    array[k] = L[ii];
+                    ii++;
+                }
+                else
+                {
+                    array[k] = R[jj];
+                    jj++;
+                }
+                k++;
+            }
+
+            while (ii < length1)
+            {
+                array[k] = R[ii];
+                ii++;
+                k++;
+            }
+            while (jj < length2)
+            {
+                array[k] = R[jj];
+                jj++;
+                k++;
+            }
+        }
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Sort an array with the Merge Sort algorithm using the left-most index l and right-most index r as parameters.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="l"></param>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public static int[] MergeSort(int[] array, int l, int r)
+        {
+            if(l < r)
+            {
+                int m = (l + r) / 2;
+                MergeSort(array, l, m);
+                MergeSort(array, m + 1, r);
+                Merge(array, l, m, r);
+            }
+            return array;
+        }
+
         /// <summary>
         /// Perform binary search on an input array and returns the index of the target if it exists, otherwise returns -1.
         /// </summary>
@@ -19,7 +106,7 @@ namespace leetcode_playground
             int L = 0;
             int R = array.Length - 1;
 
-            while(L <= R)
+            while (L <= R)
             {
                 int mid = (L + R) / 2;
                 if (array[mid] < target)
@@ -30,17 +117,6 @@ namespace leetcode_playground
                     return mid;
             }
             return -1;
-        }
-
-        private static int guess(int num)
-        {
-            int pick = 1702766719;
-            if (num > pick)
-                return -1;
-            else if (num < pick)
-                return 1;
-            else
-                return 0;
         }
 
         public static int GuessNumber(int upperBound)
@@ -73,5 +149,6 @@ namespace leetcode_playground
 
             }
         }
+        #endregion
     }
 }
